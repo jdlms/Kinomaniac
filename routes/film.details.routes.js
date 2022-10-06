7df7046176a863248eb9e31a561a414fcf786a0d
jdlms <97018563+jdlms@users.noreply.github.com>
2022-10-06 11:37:15 +0200

    titles and graph to details page

--- routes/film.details.routes.js
+++ routes/film.details.routes.js
@@ -7,10 +7,14 @@ const moviedb = new MovieDb(process.env.KEY);
 router.get("/film-details/:id", async (req, res) => {
   try {
     const data = await moviedb.movieInfo({ id: req.params.id });
-    console.log(data);
+    console.log(data.original_language);
 
-    res.render("film-details");
+    res.render("film-details", { data });
   } catch (error) {}
 });
+
+//to include in details page, from movieInfo api call: original_title, overview, poster_path, genres, tagline
+
+//info to include that is not in movieInfo api call: random image from film (?), cast, crew, details (studio, country, language, length), themes (extension of genre tag), where to watch
 
 module.exports = router;
