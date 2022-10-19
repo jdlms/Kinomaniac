20dec5c34b6fe53b022bec2852859225aa609457
jdlms <lamorey.joshua@gmail.com>
2022-10-19 16:55:32 +0200

    hide icons on login

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,7 +1,5 @@
 const router = require("express").Router();
 const { MovieDb } = require("moviedb-promise");
-
-//api key
 const moviedb = new MovieDb(process.env.KEY);
 
 /* GET home page */
@@ -9,13 +7,11 @@ router.get("/", async (req, res, next) => {
   try {
     const data = await moviedb.trending({ media_type: "movie", time_window: "week" });
     const data_current = await moviedb.movieNowPlaying({ page: 1, region: "DE" });
-    console.log(data_current);
     const config = await moviedb.configuration();
     const configCall = config.images;
     const configString = configCall.base_url + configCall.poster_sizes[2];
     //map img link/string into each movie object
     data.results.map((movie) => (movie.first_url_string = configString));
-    // console.log(config);
     //get larger image for header
     const largerImg = configCall.base_url + configCall.backdrop_sizes[2];
     data.results.map((movie) => (movie.large_img_url_string = largerImg));
@@ -24,10 +20,16 @@ router.get("/", async (req, res, next) => {
     const randomMovieIndex = Math.floor(Math.random() * data.results.length);
     const randomMovie = data.results[randomMovieIndex];
 
+    let notLoggedIn = false;
+    if (req.isAuthenticated()) {
+      notLoggedIn = true;
+    }
+    console.log(notLoggedIn);
     res.render("index", {
       docTitle: "Kinomaniacs",
       data: data.results,
       randomMovie,
+      notLoggedIn,
     });
   } catch (error) {
     res.render("error");
