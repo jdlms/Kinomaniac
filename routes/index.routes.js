f880a4491605d9cfa0dd65be99a5e960c9daac2b
jdlms <97018563+jdlms@users.noreply.github.com>
2022-10-21 22:02:57 +0200

    movies on index now tile evenly

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -6,23 +6,27 @@ const moviedb = new MovieDb(process.env.KEY);
 /* GET home page */
 router.get("/", async (req, res, next) => {
   try {
-    const data = await moviedb.trending({ media_type: "movie", time_window: "week" });
+    const data1 = await moviedb.trending({ media_type: "movie", time_window: "week" });
+    const data2 = await moviedb.trending({ media_type: "movie", time_window: "week", page: 2 });
 
     const data_current = await moviedb.movieNowPlaying({ page: 1, region: "DE" });
-    console.log(data_current);
-    
+    // console.log(data_current);
+
+    const data = [...data1.results, ...data2.results];
+    data.splice(data.length - 4, 4);
+
     const config = await moviedb.configuration();
     const configCall = config.images;
     const configString = configCall.base_url + configCall.poster_sizes[2];
     //map img link/string into each movie object
-    data.results.map((movie) => (movie.first_url_string = configString));
+    data.map((movie) => (movie.first_url_string = configString));
     //get larger image for header
     const largerImg = configCall.base_url + configCall.backdrop_sizes[2];
-    data.results.map((movie) => (movie.large_img_url_string = largerImg));
+    data.map((movie) => (movie.large_img_url_string = largerImg));
 
     //get random image from trending movies
-    const randomMovieIndex = Math.floor(Math.random() * data.results.length);
-    const randomMovie = data.results[randomMovieIndex];
+    const randomMovieIndex = Math.floor(Math.random() * data.length);
+    const randomMovie = data[randomMovieIndex];
 
     const usermoviedata = await UserMovieData.find({ userId: req.user?.googleId });
     const userMoviesById = usermoviedata.reduce((acc, val) => {
@@ -32,7 +36,7 @@ router.get("/", async (req, res, next) => {
 
     res.render("index", {
       docTitle: "Kinomaniacs",
-      data: data.results,
+      data,
       randomMovie,
       userMoviesById,
     });
