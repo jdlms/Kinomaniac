ea505f4bd8dcc5ab87e1963849b609f48475643b
jdlms <97018563+jdlms@users.noreply.github.com>
2022-10-24 23:23:05 +0200

    basic upcoming route and DE US

--- app.js
+++ app.js
@@ -45,6 +45,9 @@ app.use(user);
 const reviews = require("./routes/review.routes");
 app.use(reviews);
 
+const upcoming = require("./routes/upcoming.routes");
+app.use("/", upcoming);
+
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require("./error-handling")(app);
 
